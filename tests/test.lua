1c9c994126a48057a69bfa7f00f21319c486309c
David Manura <dm.git@math2.org>
2012-04-17 21:15:08 -0400

    test: check incremental sumhexa

--- tests/test.lua
+++ tests/test.lua
@@ -4,6 +4,11 @@
 
 local md5 = require"md5"
 
+local function checkeq(a, b)
+  if a ~= b then
+    error('noteequal:' .. tostring(a) .. ' ' .. tostring(b))
+  end
+end
 
 assert(md5.exor('', '') == '')
 assert(md5.exor('alo alo', '\0\0\0\0\0\0\0') == 'alo alo')
@@ -20,29 +25,44 @@ assert(invert(invert('alo alo')) == 'alo alo')
 
 assert(invert(invert(invert('alo\0\255alo'))) == invert('alo\0\255alo'))
 
+-- compute sumhexa multiple ways
+local function sumhexa(s)
+  -- compute as single string
+  local sum1 = md5.sumhexa(s)
+  -- compute incrementally
+  local ctx = true
+  for i=1,#s+1,64 do
+    ctx = md5.sumhexa(s:sub(i,i+63), ctx)
+  end
+  local sum2 = ctx
+  -- compare and return
+  checkeq(sum1, sum2)
+  return sum1
+end
+
 -- test some known sums
-assert(md5.sumhexa("") == "d41d8cd98f00b204e9800998ecf8427e")
-assert(md5.sumhexa("a") == "0cc175b9c0f1b6a831c399e269772661")
-assert(md5.sumhexa("abc") == "900150983cd24fb0d6963f7d28e17f72")
-assert(md5.sumhexa("message digest") == "f96b697d7cb7938d525a2f31aaf161d0")
-assert(md5.sumhexa("abcdefghijklmnopqrstuvwxyz") == "c3fcd3d76192e4007dfb496cca67e13b")
-assert(md5.sumhexa("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+assert(sumhexa("") == "d41d8cd98f00b204e9800998ecf8427e")
+assert(sumhexa("a") == "0cc175b9c0f1b6a831c399e269772661")
+assert(sumhexa("abc") == "900150983cd24fb0d6963f7d28e17f72")
+assert(sumhexa("message digest") == "f96b697d7cb7938d525a2f31aaf161d0")
+assert(sumhexa("abcdefghijklmnopqrstuvwxyz") == "c3fcd3d76192e4007dfb496cca67e13b")
+assert(sumhexa("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
  == "d174ab98d277d9f5a5611c2c9f419d9f")
 
 
 -- test padding borders
-assert(md5.sumhexa(string.rep('a',53)) == "e9e7e260dce84ffa6e0e7eb5fd9d37fc")
-assert(md5.sumhexa(string.rep('a',54)) == "eced9e0b81ef2bba605cbc5e2e76a1d0")
-assert(md5.sumhexa(string.rep('a',55)) == "ef1772b6dff9a122358552954ad0df65")
-assert(md5.sumhexa(string.rep('a',56)) == "3b0c8ac703f828b04c6c197006d17218")
-assert(md5.sumhexa(string.rep('a',57)) == "652b906d60af96844ebd21b674f35e93")
-assert(md5.sumhexa(string.rep('a',63)) == "b06521f39153d618550606be297466d5")
-assert(md5.sumhexa(string.rep('a',64)) == "014842d480b571495a4a0363793f7367")
-assert(md5.sumhexa(string.rep('a',65)) == "c743a45e0d2e6a95cb859adae0248435")
-assert(md5.sumhexa(string.rep('a',255)) == "46bc249a5a8fc5d622cf12c42c463ae0")
-assert(md5.sumhexa(string.rep('a',256)) == "81109eec5aa1a284fb5327b10e9c16b9")
+assert(sumhexa(string.rep('a',53)) == "e9e7e260dce84ffa6e0e7eb5fd9d37fc")
+assert(sumhexa(string.rep('a',54)) == "eced9e0b81ef2bba605cbc5e2e76a1d0")
+assert(sumhexa(string.rep('a',55)) == "ef1772b6dff9a122358552954ad0df65")
+assert(sumhexa(string.rep('a',56)) == "3b0c8ac703f828b04c6c197006d17218")
+assert(sumhexa(string.rep('a',57)) == "652b906d60af96844ebd21b674f35e93")
+assert(sumhexa(string.rep('a',63)) == "b06521f39153d618550606be297466d5")
+assert(sumhexa(string.rep('a',64)) == "014842d480b571495a4a0363793f7367")
+assert(sumhexa(string.rep('a',65)) == "c743a45e0d2e6a95cb859adae0248435")
+assert(sumhexa(string.rep('a',255)) == "46bc249a5a8fc5d622cf12c42c463ae0")
+assert(sumhexa(string.rep('a',256)) == "81109eec5aa1a284fb5327b10e9c16b9")
 
-assert(md5.sumhexa(
+assert(sumhexa(
 "12345678901234567890123456789012345678901234567890123456789012345678901234567890")
    == "57edf4a22be3c955ac49da2e2107b67a")
 
