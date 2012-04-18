4508c40ccd9fd575f8813d459560f10f31c388b3
Roc King <ownwaterloo@gmail.com>
2012-04-19 04:18:02 +0800

    add necessary macros for hash_api

--- hash_api.h
+++ hash_api.h
@@ -2,6 +2,31 @@
 #define hash_api_h
 
 
+#define hash_t hash_t_(hash)
+#define hash_t_(x) hash_t_r_(x)
+#define hash_t_r_(x) x##_t
+
+#define HASH_HASHSIZE HASH_HASHSIZE_(HASH)
+#define HASH_HASHSIZE_(x) HASH_HASHSIZE_R_(x)
+#define HASH_HASHSIZE_R_(x) x##_HASHSIZE
+
+#define HASH_BLOCKSIZE HASH_BLOCKSIZE_(HASH)
+#define HASH_BLOCKSIZE_(x) HASH_BLOCKSIZE_R_(x)
+#define HASH_BLOCKSIZE_R_(x) x##_BLOCKSIZE
+
+#define hash_init hash_init_(hash)
+#define hash_init_(x) hash_init_r_(x)
+#define hash_init_r_(x) x##_init
+
+#define hash_update hash_update_(hash)
+#define hash_update_(x) hash_update_r_(x)
+#define hash_update_r_(x) x##_update
+
+#define hash_finish hash_finish_(hash)
+#define hash_finish_(x) hash_finish_r_(x)
+#define hash_finish_r_(x) x##_finish
+
+
 #define DEFINE_HASH_API(hash, HASH) \
 void hash##_init(hash##_t *m) { \
   init_digest(m->d); \
@@ -55,5 +80,32 @@ void hash(const char *message, size_t len, \
   hash##_finish(&m, output); \
 }
 
+
+#undef hash_t
+#undef hash_t_
+#undef hash_t_r_
+
+#undef HASH_HASHSIZE
+#undef HASH_HASHSIZE_
+#undef HASH_HASHSIZE_R_
+
+#undef HASH_BLOCKSIZE
+#undef HASH_BLOCKSIZE_
+#undef HASH_BLOCKSIZE_R_
+
+#undef hash_init
+#undef hash_init_
+#undef hash_init_r_
+
+#undef hash_update
+#undef hash_update_
+#undef hash_update_r_
+
+#undef hash_finish
+#undef hash_finish_
+#undef hash_finish_r_
+
+#undef hash
+#undef HASH
 
 #endif /* hash_api_h */
